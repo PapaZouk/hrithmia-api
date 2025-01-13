fed59708f3a3b5deb704b2a3b6b0404b30fbdae5
PapaZouk <rafexpapala@gmail.com>
2025-01-13 21:06:20 +0100

    feat: update update employee by id endpoint

--- src/controllers/employee/updateEmployeeById.ts
+++ src/controllers/employee/updateEmployeeById.ts
@@ -1,68 +1,89 @@
-import Employee, {IEmployee} from '../../model/IEmployee.ts';
-import {connectDb} from "../../database/connectDb.ts";
-import {Context} from "hono";
+import Employee, { IEmployee } from "../../model/IEmployee.ts";
+import { connectDb } from "../../database/connectDb.ts";
+import { Context, TypedResponse } from "hono";
 
-export const updateEmployeeById = async (c: Context) => {
-    console.log("Requesting to update employee");
-    try {
-        await connectDb();
+export const updateEmployeeById = async (
+  c: Context,
+): Promise<TypedResponse> => {
+  console.log("Requesting to update employee");
+  try {
+    await connectDb();
 
-        const id = c.req.param("id");
+    const id = c.req.param("id");
 
-        if (!id) {
-            c.status(400);
-            return c.json({ message: "ID is required" });
-        }
-
-        const data = await c.req.json();
-
-        const employee: IEmployee|null = await Employee.findById(id);
-
-        if (!employee) {
-            c.status(404);
-            return c.json({ message: "Employee not found" });
-        }
-
-        console.log("Updating employee data");
-        updateEmployeeData(employee, data);
-
-        await employee.save();
-
-        return c.json({ message: "Employee updated" });
-    } catch (error) {
-        console.error((error as Error).message);
-        return c.json({ error: (error as Error).message });
+    if (!id) {
+      c.status(400);
+      return c.json({ message: "ID is required" });
     }
-}
+
+    const data = await c.req.json();
+
+    const employee: IEmployee | null = await Employee.findById(id);
+
+    if (!employee) {
+      c.status(404);
+      return c.json({ message: "Employee not found" });
+    }
+
+    console.log("Updating employee data");
+    updateEmployeeData(employee, data);
+
+    await employee.save();
+
+    return c.json({ message: "Employee updated" });
+  } catch (error) {
+    console.error((error as Error).message);
+    return c.json({ error: (error as Error).message });
+  }
+};
 
 const updateEmployeeData = (employee: IEmployee, data: any) => {
-    if (data.personalData) {
-        employee.personalData = {
-            ...employee.personalData,
-            ...data.personalData,
-            address1: {
-                ...employee.personalData.address1,
-                ...data.personalData.address1,
-            },
-            address2: {
-                ...employee.personalData.address2,
-                ...data.personalData.address2,
-            }
-        };
-    }
+  if (data.personalData) {
+    employee.personalData = {
+      ...employee.personalData,
+      ...data.personalData,
+      address1: {
+        ...employee.personalData.address1,
+        ...data.personalData.address1,
+      },
+      address2: {
+        ...employee.personalData.address2,
+        ...data.personalData.address2,
+      },
+    };
+  }
 
-    if (data.jobDetails) {
-        employee.jobDetails = {
-            ...employee.jobDetails,
-            ...data.jobDetails,
-            salary: {
-                ...employee.jobDetails?.salary,
-                ...data.jobDetails?.salary,
-                salaryHistory: [
-                    ...employee.jobDetails?.salary?.salaryHistory,
-                    ...data.jobDetails?.salary?.salaryHistory
-                ]
-            }
-        };
-    }
+  if (data.jobDetails) {
+    employee.jobDetails = {
+      ...employee.jobDetails,
+      ...data.jobDetails,
+    };
+  } else {
+    employee.jobDetails = data.jobDetails;
+  }
+
+  if (data.jobDetails?.salary) {
+    const existingSalaryHistory = employee.jobDetails?.salary?.salaryHistory ||
+      [];
+    const newSalaryHistory = data.jobDetails.salary.salaryHistory || [];
+
+    const mergedSalaryHistory = [
+      ...existingSalaryHistory,
+      ...newSalaryHistory,
+    ];
+
+    const uniqueSalaryHistory = Array.from(
+      new Map(mergedSalaryHistory.map((item) => [item.changeDate, item]))
+        .values(),
+    );
+
+    employee.jobDetails = {
+      ...employee.jobDetails,
+      salary: {
+        ...employee.jobDetails?.salary,
+        ...data.jobDetails.salary,
+        salaryHistory: uniqueSalaryHistory,
+      },
+    };
+  }
 };
